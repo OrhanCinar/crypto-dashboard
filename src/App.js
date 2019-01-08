9ab8a63070d65ad7e2c9e940bbd64aa37699b650
Orhan CINAR <orhancinar35@gmail.com>
2019-01-08 01:40:52 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+
 
 class App extends Component {
   render() {
@@ -19,6 +21,8 @@ class App extends Component {
           >
             Learn React
           </a>
+
+          <Button color="danger">Danger!</Button>
         </header>
       </div>
     );
