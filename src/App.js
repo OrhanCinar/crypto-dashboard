49f3e53e6cded5d64d9e71a542e7d077ab0bcf07
Orhan CINAR <orhancinar35@gmail.com>
2019-01-10 03:26:22 +0100

    websocket implementation started

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'reactstrap';
+import React, { Component } from "react";
+import CoinCard from "./components/Coincard";
 
+import { Container, Row, Col } from "reactstrap";
+
+import startWebSocket from "./components/binance-websocket"
+
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
+  state = {
+    coins: [{ id: "ETHBTC", price: 0 }]
+  };
+
+  componentDidMount() {
+    this.startWebSocket();
+  }
+
+  startWebSocket() {
+    this.ws = new WebSocket(
+      "wss://stream.binance.com:9443/ws/ethbtc@miniTicker"
+      //"wss://stream.binance.com:9443/ws/!miniTicker@arr"
+    );
+
+    this.ws.onopen = () => {
+      console.log("opening");
+    };
+
+    this.ws.onclose = () => {
+      console.log("closing");
+    };
+
+    this.ws.onerror = event => {
+      console.log(`Error : ${event}`);
+    };
+
+    this.ws.onmessage = e => {
+      const data = JSON.parse(e.data);
+      const coinName = data.s;
+
+      //console.log("Coin" + coinName);
+
+      const price = data.c;
+      const coins = [...this.state.coins];
+
+      const coin = coins.find(c => c.id === coinName);
+
+      if (coin.price !== price) {
+        coin.price = price;
+        this.setState = { coins };
+        this.forceUpdate();
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        {
+          <React.Fragment>
+            <Container>
+              <Row>
+                <Col>TOP</Col>
+              </Row>
+            </Container>
 
-          <Button color="danger">Danger!</Button>
-        </header>
+            <CoinCard coins={this.state.coins} />
+
+            <Container />
+          </React.Fragment>
+        }
       </div>
     );
   }
