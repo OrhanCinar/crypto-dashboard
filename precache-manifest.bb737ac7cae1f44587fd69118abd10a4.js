bf6d97eedd1c4429da1ca20c840bdada21287219
Orhan CINAR <orhancinar35@gmail.com>
2019-01-11 03:47:43 +0100

    Updates

---
+++ precache-manifest.bb737ac7cae1f44587fd69118abd10a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ca523a0e76b36b7ceb2",
+    "url": "/crypto-dashboard/static/js/runtime~main.8ca523a0.js"
+  },
+  {
+    "revision": "3cd47c731832c4e36fc3",
+    "url": "/crypto-dashboard/static/js/main.3cd47c73.chunk.js"
+  },
+  {
+    "revision": "11dec9d40344b5964383",
+    "url": "/crypto-dashboard/static/js/1.11dec9d4.chunk.js"
+  },
+  {
+    "revision": "3cd47c731832c4e36fc3",
+    "url": "/crypto-dashboard/static/css/main.e92e5ffb.chunk.css"
+  },
+  {
+    "revision": "11dec9d40344b5964383",
+    "url": "/crypto-dashboard/static/css/1.19e93ddc.chunk.css"
+  },
+  {
+    "revision": "74a062a41fac6c95ec949fa10d204eac",
+    "url": "/crypto-dashboard/index.html"
+  }
+];
